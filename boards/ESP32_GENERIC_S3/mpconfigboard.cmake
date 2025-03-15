fdb0fa2252845d6b74c69030963d1806431f8053
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2025-03-15 16:50:04 +0100

    mpconfigboard.cmake aktualisieren

--- boards/ESP32_GENERIC_S3/mpconfigboard.cmake
+++ boards/ESP32_GENERIC_S3/mpconfigboard.cmake
@@ -1,5 +1,7 @@
 set(IDF_TARGET esp32s3)
 
+message(STATUS "MICROPY_PORT_DIR is set to ${MICROPY_PORT_DIR}")
+
 set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.base
     ${MICROPY_PORT_DIR}/boards/sdkconfig.usb
