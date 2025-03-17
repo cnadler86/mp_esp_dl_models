742725e7ba1905702fbcbb382578d27b5601d430
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2025-03-17 07:25:21 +0100

    mpconfigvariant_FLASH_16M.cmake aktualisieren

--- boards/ESP32_GENERIC_S3/mpconfigvariant_FLASH_16M.cmake
+++ boards/ESP32_GENERIC_S3/mpconfigvariant_FLASH_16M.cmake
@@ -2,7 +2,7 @@ set(SDKCONFIG_DEFAULTS
     ${SDKCONFIG_DEFAULTS}
     ${MICROPY_PORT_DIR}/boards/sdkconfig.240mhz
     ${MICROPY_PORT_DIR}/boards/sdkconfig.spiram_oct
-    boards/ESP32_GENERIC_S3/sdkconfig.flash_16m
+    ESP32_GENERIC_S3/sdkconfig.flash_16m
 )
 
 list(APPEND MICROPY_DEF_BOARD
